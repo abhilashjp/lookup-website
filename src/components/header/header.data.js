d055f23b6a40704fc7a652a4614dbf03eb499578
Abhilash John <abhilashjohn07@gmail.com>
2022-07-26 21:02:29 +0530

    Update header.data.js

--- src/components/header/header.data.js
+++ src/components/header/header.data.js
@@ -1,8 +1,14 @@
 import Link from 'components/link';
 
-<h1 className="title">
+export default function FirstPost() {
+  return (
+    <>
+      <h1 className="title">
   Read{' '}
   <Link href="/docs/intro">
     <a>this page!</a>
   </Link>
 </h1>
+    </>
+  );
+}
