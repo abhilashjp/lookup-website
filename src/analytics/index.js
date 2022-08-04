539aa96ed550a1c08c3bf696ec505277a93c6e1e
Abhilash John <abhilashjohn07@gmail.com>
2022-08-04 23:49:08 +0530

    Update index.js

--- src/analytics/index.js
+++ src/analytics/index.js
@@ -2,7 +2,7 @@ import ReactGA from 'react-ga';
 
 export const initGA = () => {
   console.log('GA init');
-  ReactGA.initialize('UA-xxxxxxxxx-1');
+  ReactGA.initialize('G-D5YNWHF9Q3');
 };
 
 export const logPageView = () => {
