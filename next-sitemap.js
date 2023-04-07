ab3f21e46c76278495c1a634a1450e7b71d8757b
Abhilash John <abhilashjohn07@gmail.com>
2023-04-07 17:51:02 +0530

    Update next-sitemap.js

--- next-sitemap.js
+++ next-sitemap.js
@@ -2,11 +2,11 @@ module.exports = {
   siteUrl: 'https://lookuptax.com/',
   generateRobotsTxt: true,
   // optional
-  // robotsTxtOptions: {
-  //   additionalSitemaps: [
-  //     'https://example.com/my-custom-sitemap-1.xml',
+  robotsTxtOptions: {
+   additionalSitemaps: [
+  //     'https://lookuptax.com/docs/sitemap.xml',
   //     'https://example.com/my-custom-sitemap-2.xml',
   //     'https://example.com/my-custom-sitemap-3.xml',
-  //   ],
-  // },
+   ],
+  },
 };
