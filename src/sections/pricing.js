11441f6b9418bcb5bd70d84dc5ef0e7c867073db
Abhilash John <abhilashjohn07@gmail.com>
2024-03-08 10:34:57 +0530

    Update pricing.js

--- src/sections/pricing.js
+++ src/sections/pricing.js
@@ -225,7 +225,7 @@ const Pricing = () => {
         <iframe
           src={`https://share.hsforms.com/1oW99rVKyRFCNhfrG2nVi4Adew82`}
           width="100%"
-          //height="100%"
+          
           frameBorder="0"
           title="HubSpot Form"
         />
