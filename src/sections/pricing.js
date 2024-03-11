d457db7339c6fe4111426a6a22bb090811be57c3
Abhilash John <abhilashjohn07@gmail.com>
2024-03-11 13:10:22 +0530

    Update pricing.js

--- src/sections/pricing.js
+++ src/sections/pricing.js
@@ -304,18 +304,7 @@ const styles = {
       },
     },
   },
-modal: {
-  /* backgroundColor: 'rgba(0, 0, 0, 0.8)', */
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    zIndex: 9999,
-  },
+
   modalBody: {
     backgroundColor: 'white',
     padding: '20px',
