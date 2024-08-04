d8f0108a3299216bb728a218bf351f3d5ac8dc2f
Sujith G <sujith3g@gmail.com>
2024-08-04 18:56:03 +0530

    Add format validated response to demo form

--- src/components/card-demo-response.js
+++ src/components/card-demo-response.js
@@ -2,32 +2,55 @@ import React, { useState } from 'react';
 import {Alert, Box, Grid, Text} from 'theme-ui';
 
 const CardDemoResponse = ({response}) => {
-  return (
+
+  if (response.valid) {
+    return (
+      <Box>
+          <Box>
+            <Alert variant='highlight' mb={3}>Valid Tax ID Number</Alert>
+            <Grid gap={2} columns={[2, '2fr 3fr']}>
+              <Text sx={styles.fieldlabel}> Valid </Text>
+              <Text sx={styles.fieldvalue}> {response.valid.toString()} </Text>
+              <Text sx={styles.fieldlabel}> Tax ID Label </Text>
+              <Text sx={styles.fieldvalue}> {response.tinLabel} </Text>
+              <Text sx={styles.fieldlabel}> Tax ID Number </Text>
+              <Text sx={styles.fieldvalue}> {response.tin} </Text>
+              <Text sx={styles.fieldlabel}> Name </Text>
+              <Text sx={styles.fieldvalue}> {response.name} </Text>
+              <Text sx={styles.fieldlabel}> Address </Text>
+              <Text sx={styles.fieldvalue}> {response.address} </Text>
+              <Text sx={styles.fieldlabel}> Date </Text>
+              <Text sx={styles.fieldvalue}> {response.requestDate} </Text>
+            </Grid>
+          </Box>
+      </Box>
+    );
+  } else if (response.isFormatValid) {
+    return (
+      <Box>
+          <Box>
+            <Alert variant='highlight' mb={3}>Valid Tax ID Number</Alert>
+            <Grid gap={2} columns={[2, '2fr 3fr']}>
+              <Text sx={styles.fieldlabel}> Valid Format </Text>
+              <Text sx={styles.fieldvalue}> {response.isFormatValid.toString()} </Text>
+              <Text sx={styles.fieldlabel}> Tax ID Label </Text>
+              <Text sx={styles.fieldvalue}> {response.tinLabel} </Text>
+              <Text sx={styles.fieldlabel}> Tax ID Number </Text>
+              <Text sx={styles.fieldvalue}> {response.tin} </Text>
+              <Text sx={styles.fieldlabel}> Date </Text>
+              <Text sx={styles.fieldvalue}> {response.requestDate} </Text>
+            </Grid>
+          </Box>
+      </Box>    );
+  } else {
+    return (
     <Box>
-    { response.valid ? (
-        <Box>
-          <Alert variant='highlight' mb={3}>Valid Tax ID Number</Alert>
-          <Grid gap={2} columns={[2, '2fr 3fr']}>
-            <Text sx={styles.fieldlabel}> Valid </Text>
-            <Text sx={styles.fieldvalue}> {response.valid.toString()} </Text>
-            <Text sx={styles.fieldlabel}> Tax ID Number </Text>
-            <Text sx={styles.fieldvalue}> {response.vatNumber} </Text>
-            <Text sx={styles.fieldlabel}> Name </Text>
-            <Text sx={styles.fieldvalue}> {response.name} </Text>
-            <Text sx={styles.fieldlabel}> Address </Text>
-            <Text sx={styles.fieldvalue}> {response.address} </Text>
-            <Text sx={styles.fieldlabel}> Date </Text>
-            <Text sx={styles.fieldvalue}> {response.requestDate} </Text>
-          </Grid>
-        </Box>
-      ) : (
-        <Box>
+      <Box>
           <Alert bg='red' mb={3}>Invalid Tax ID Number</Alert>
         </Box>
-      )
-    }
     </Box>
-  );
+    );
+  }
 }
 
 const styles = {
