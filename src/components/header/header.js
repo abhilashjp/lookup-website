201fd0d8b019e4aca0d471b1ff0b46236e12c4d2
Abhilash John <abhilashjohn07@gmail.com>
2024-08-18 13:40:40 +0530

    Update header.js

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -55,7 +55,16 @@ export default function Header() {
               <script async src="https://js.stripe.com/v3/pricing-table.js"></script>
         }
 
-       
+        {
+         //   <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KLGV623');</script>
+//<!-- End Google Tag Manager -->
+        }
+
 
             <Box sx={styles.headerInner}>
               <Logo isWhite={mobileMenu} />
